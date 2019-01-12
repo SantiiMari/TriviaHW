40b4cf54b57fdec258ce1ce01d4c76cf8f537d8a
SantiimariSantiimarigit configgit config --global user.name :Santiimari <MaryF.Santibanez@gmail.com>
2019-01-11 18:52:15 -0600

    added q's

---
+++ javascript.js
@@ -0,0 +1,54 @@
+
+
+
+$(document).ready(function () {
+ 
+    var correctGuesses = 0;
+    var incorrectGuesses = 0;
+    var questionCounter = 0;
+    var time = 15;
+    
+    
+  
+    
+
+ 
+    var questions = [
+        {
+            question: "Who is the main character of Uncharted?",
+            choices: ["Nathan Drake", "Aloy", "Master Chief", "Mario"],
+            correctAnswer: "Nathan Drake",
+        },
+
+
+        {
+            question: "What Pokemon does Pikachu evolve into?",
+            choices: ["Jolteon", "Pichu", "Raichu", "Mimikyu"],
+            correctAnswer: "Raichu",
+        },
+
+        {
+            question: "How many Gym Badges must a trainer collect before challenging the Elite Four?",
+            choices: ["6", "8", "9"," 10"],
+            correctAnswer: "8"
+        },
+
+        {
+
+            question: "What's the device trainers use to keep a record of their Pokemon encounters?",
+            choices: ["Pokedex", "Pokecorder", "Pokefinder", "Pokephone"],
+            correctAnswer: "Pokedex",
+        },
+
+        {
+
+            question: "Which of these is a starter pokemon?",
+            choices: ["Pikachu", "Mudkip", "Growlithe", "Meowth"],
+            correctAnswer: "Mudkip",
+        },
+
+    ];
+
+
+   
+});
